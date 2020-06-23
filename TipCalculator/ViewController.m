aba4046dc577485ac3e562127996311cead71bd3
Pranathi Peri <pranathiperi@fb.com>
2020-06-23 16:05:38 -0500

    Initial commit of basic app

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
 @end
 
 @implementation ViewController
@@ -18,6 +23,51 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello");
+    // in java --> self.view.endEditing(YES);
+    [self.view endEditing:YES];
+}
 
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    NSArray *percentages = @[@(0.15), @(0.2), @(0.22)];
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+    
+}
+
+- (IBAction)onEditingBegin:(id)sender {
+    
+    CGRect newFrame = self.billField.frame;
+    newFrame.origin.y +=30;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = newFrame;
+    }];
+    
+    [UIView animateWithDuration:1 animations:^{
+        self.tipLabel.alpha = 0;
+    }];
+    
+}
+- (IBAction)onEditingEnd:(id)sender {
+    
+    CGRect newFrame = self.billField.frame;
+    newFrame.origin.y -=30;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = newFrame;
+    }];
+    
+    [UIView animateWithDuration:1 animations:^{
+        self.tipLabel.alpha = 1;
+    }];
+    
+    
+}
 
 @end
